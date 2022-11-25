f0986db099dce445ecaf3ec26422861c13bf9163
Rinoreinz <angrykeo@gmail.com>
2022-11-25 23:51:02 +0300

    added test coverage

--- task1.py
+++ task1.py
@@ -3,7 +3,7 @@ import requests as req
 
 
 def check_link(link:str):
-    link_ref = "^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?"
+    link_ref = "^((ftp|http|https):\/\/)(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?"
 
     if re.match(link_ref, link) is not None:
         return True
@@ -14,15 +14,15 @@ def link_dict(link_list):
     link_dict = {}
     for link in link_list:
         method_dict = {}
-        if check_link(link) == False:
-            print(f'Строка {link} не является ссылкой')
-
-        else:
+        if check_link(link) != False:
             if  (req.get(link).status_code) != 405:
                 method_dict['get'] = req.get(link).status_code
             elif (req.post(link).status_code) != 405:
                 method_dict['post'] = req.post(link).status_code
-        link_dict[link] = method_dict
+            link_dict[link] = method_dict
+        else:
+            print(f'Строка {link} не является ссылкой')
+
     return link_dict
 
 
--- task1_test.py
+++ task1_test.py
@@ -3,22 +3,28 @@ from task1 import check_link, link_dict
 
 
 
-def test_check_link():
+def test_check_link_success():
         link = 'https://google.com'
         result = bool(check_link(link))
 
         assert result == True
 
+
+def test_check_link_wrong():
+        link = 'google.com'
+        result = bool(check_link(link))
+
+        assert result != True
 def test_success_link_dict():
-        link = 'https://google.com'
+        link = ['https://google.com']
         result = link_dict(link)
 
         assert   result == {'https://google.com': {'get': 200}}
 
 def test_wrong_link_dict():
-        link = "hello"
+        link = ["hello"]
         result = link_dict(link)
         print(result)
-        ref = "Cтрока hello не является ссылкой"
-        assert result != ref
+        ref = {}
+        assert result == ref
 
